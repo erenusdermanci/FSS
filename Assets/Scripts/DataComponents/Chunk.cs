5b25d4b28c1de6cd8662a096caececa9ece94a62
Erenus Dermanci <erenus@mail.com>
2021-03-03 18:23:07 +0100

    Revert a few changes to configs

--- Assets/Scripts/DataComponents/Chunk.cs
+++ Assets/Scripts/DataComponents/Chunk.cs
@@ -5,7 +5,7 @@ namespace DataComponents
 {
     public class Chunk : IDisposable
     {
-        public static readonly int Size = 16;
+        public static readonly int Size = 64;
 
         public Texture2D Texture;
         public GameObject GameObject;
