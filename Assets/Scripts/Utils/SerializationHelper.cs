74109c97b2eba12d99322babb6b31a8f1676c520
ryd <42.rdavid@gmail.com>
2021-03-05 12:34:43 +0100

    Add serialization helper

---
+++ Assets/Scripts/Utils/SerializationHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+namespace Utils
+{
+    public class SerializationHelper
+    {
+        /// <summary>
+        /// Serializes an object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serializableObject"></param>
+        /// <param name="fileName"></param>
+        public static void SerializeObject<T>(T serializableObject, string fileName)
+        {
+            if (serializableObject == null) { return; }
+
+            try
+            {
+                var xmlDocument = new XmlDocument();
+                var serializer = new XmlSerializer(serializableObject.GetType());
+                using (var stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, serializableObject);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(Application.persistentDataPath + "\\" + fileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Exception while serializing " + fileName);
+                Debug.Log(ex);
+            }
+        }
+
+
+        /// <summary>
+        /// Deserializes an xml file into an object list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static T DeSerializeObject<T>(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) { return default(T); }
+
+            var objectOut = default(T);
+
+            try
+            {
+                var xmlDocument = new XmlDocument();
+                xmlDocument.Load(Application.persistentDataPath + "\\" + fileName);
+                var xmlString = xmlDocument.OuterXml;
+
+                using (var read = new StringReader(xmlString))
+                {
+                    var outType = typeof(T);
+
+                    var serializer = new XmlSerializer(outType);
+                    using (XmlReader reader = new XmlTextReader(read))
+                    {
+                        objectOut = (T)serializer.Deserialize(reader);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Exception while deserializing " + fileName);
+                Debug.Log(ex);
+            }
+
+            return objectOut;
+        }
+    }
+}
