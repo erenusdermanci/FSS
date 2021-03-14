cdced2be3b426728beca37510cba3b08642aeb7b
ryd <42.rdavid@gmail.com>
2021-03-14 14:37:41 +0100

    Removed useless behavioral tags
    We need to decide what they would be useful for
    Until then its clutter

--- Assets/Scripts/Blocks/BlockDescriptor.cs
+++ Assets/Scripts/Blocks/BlockDescriptor.cs
@@ -8,7 +8,6 @@ namespace Blocks
         public readonly string Name;
         public readonly int Id;
         public readonly BlockTags PhysicalBlockTag;
-        public readonly BlockTags[] BehavioralTags;
         public readonly float DensityPriority;
         public readonly Color32 Color;
         public readonly float ColorMaxShift;
@@ -19,7 +18,6 @@ namespace Blocks
         public BlockDescriptor(string name,
             int id,
             BlockTags physicalBlockTag,
-            BlockTags[] behavioralTags,
             float densityPriority,
             Color32 color,
             float colorMaxShift,
@@ -30,7 +28,6 @@ namespace Blocks
             Name = name;
             Id = id;
             PhysicalBlockTag = physicalBlockTag;
-            BehavioralTags = behavioralTags;
             DensityPriority = densityPriority;
             Color = color;
             ColorMaxShift = colorMaxShift;
