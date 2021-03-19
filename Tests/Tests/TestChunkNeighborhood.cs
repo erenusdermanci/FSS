eae201f8922990d16e75564f9a0770505545468c
ryd <42.rdavid@gmail.com>
2021-03-19 18:01:40 +0100

    Add a few critical code benchmarks

--- Tests/Tests/TestChunkNeighborhood.cs
+++ Tests/Tests/TestChunkNeighborhood.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using Blocks;
 using Chunks;
 using NUnit.Framework;
 
@@ -7,16 +8,36 @@ namespace Tests
 {
     public class Tests
     {
+        private readonly ChunkMap<ChunkServer> _chunkMap = new ChunkMap<ChunkServer>();
+        private ChunkNeighborhood _chunkNeighborhood;
+
         [SetUp]
         public void Setup()
         {
+            var chunk = new ChunkServer();
+            chunk.Initialize();
+            chunk.GenerateEmpty();
+            var airColor = BlockConstants.BlockDescriptors[BlockConstants.Air].Color;
+            for (var i = 0; i < Chunk.Size * Chunk.Size; i++)
+            {
+                chunk.Data.colors[i * 4] = airColor.r;
+                chunk.Data.colors[i * 4 + 1] = airColor.g;
+                chunk.Data.colors[i * 4 + 2] = airColor.b;
+                chunk.Data.colors[i * 4 + 3] = airColor.a;
+                chunk.Data.types[i] = BlockConstants.Air;
+                chunk.Data.stateBitsets[i] = 0;
+                chunk.Data.healths[i] = BlockConstants.BlockDescriptors[BlockConstants.Air].BaseHealth;
+                chunk.Data.lifetimes[i] = 0;
+            }
+
+            _chunkNeighborhood = new ChunkNeighborhood(_chunkMap, chunk, new Random());
         }
 
         [Test]
         public void BenchmarkUpdateOutsideChunk()
         {
-            var rng = new System.Random();
-            var size = 4096;
+            var rng = new Random();
+            const int size = 4096;
             var xSamples = new int[size];
             var ySamples = new int[size];
             // generate valid position samples
@@ -32,9 +53,66 @@ namespace Tests
             {
                 for (var x = 0; x < size; ++x)
                 {
-                    ChunkNeighborhood.UpdateOutsideChunk(ref xSamples[x], ref ySamples[y], out var chunkIndex);
+                    ChunkNeighborhood.UpdateOutsideChunk(ref xSamples[x], ref ySamples[y], out _);
                 }
             }
+
+            sw.Stop();
+            Console.WriteLine("Took " + sw.ElapsedMilliseconds + " ms");
+        }
+
+        [Test]
+        public void BenchmarkUpdateDirtyRectForAdjacentBlock()
+        {
+            var rng = new Random();
+            const int size = 4096;
+            var xSamples = new int[size];
+            var ySamples = new int[size];
+            // generate valid position samples
+            for (var i = 0; i < size; ++i)
+            {
+                xSamples[i] = rng.Next(-Chunk.Size, Chunk.Size * 2);
+                ySamples[i] = rng.Next(-Chunk.Size, Chunk.Size * 2);
+            }
+
+            var sw = new Stopwatch();
+            sw.Start();
+            for (var y = 0; y < size; ++y)
+            {
+                for (var x = 0; x < size; ++x)
+                {
+                    _chunkNeighborhood.UpdateDirtyRectForAdjacentBlock(xSamples[x], ySamples[y]);
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine("Took " + sw.ElapsedMilliseconds + " ms");
+        }
+
+        [Test]
+        public void BenchmarkUpdateAdjacentBlockDirty()
+        {
+            var rng = new Random();
+            const int size = 4096;
+            var xSamples = new int[size];
+            var ySamples = new int[size];
+            // generate valid position samples
+            for (var i = 0; i < size; ++i)
+            {
+                xSamples[i] = rng.Next(-Chunk.Size + 1, Chunk.Size * 2 - 1);
+                ySamples[i] = rng.Next(-Chunk.Size + 1, Chunk.Size * 2 - 1);
+            }
+
+            var sw = new Stopwatch();
+            sw.Start();
+            for (var y = 0; y < size; ++y)
+            {
+                for (var x = 0; x < size; ++x)
+                {
+                    _chunkNeighborhood.UpdateAdjacentBlockDirty(xSamples[x], ySamples[y]);
+                }
+            }
+
             sw.Stop();
             Console.WriteLine("Took " + sw.ElapsedMilliseconds + " ms");
         }
