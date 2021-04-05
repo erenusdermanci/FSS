0244d5516f03e571f2a80e726a664bd6397ef728
Erenus Dermanci <erenus@mail.com>
2021-04-05 20:09:19 +0200

    Fix TileDistanceToPlayerComparer

--- Assets/Scripts/Tiles/Tasks/TileDistanceToPlayerComparer.cs
+++ Assets/Scripts/Tiles/Tasks/TileDistanceToPlayerComparer.cs
@@ -7,9 +7,11 @@ namespace Tiles.Tasks
     {
         public int Compare(Vector2i x, Vector2i y)
         {
-            var playerPosition = WorldManager.MainCameraPosition;
-            return -Vector2i.Distance(x, playerPosition.x, playerPosition.y)
-                .CompareTo(Vector2i.Distance(y, playerPosition.x, playerPosition.y));
+            var scaledX = new Vector2i(x.x * Tile.HorizontalSize, x.y * Tile.VerticalSize);
+            var scaledY = new Vector2i(y.x * Tile.HorizontalSize, y.y * Tile.VerticalSize);
+            var cameraPosition = WorldManager.MainCameraPosition;
+            return -Vector2i.Distance(scaledX, cameraPosition.x, cameraPosition.y)
+                .CompareTo(Vector2i.Distance(scaledY, cameraPosition.x, cameraPosition.y));
         }
     }
 }
